3c9ce97e6a6327d18f413bc3cf5f633b6223eafd
Nate Cox <ncox@covermymeds.com>
2024-05-30 11:30:42 -0700

    Convert elixir to devenv

---
+++ templates/dev/elixir/devenv.nix
@@ -0,0 +1,40 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "Elixir Dev";
+
+  # https://devenv.sh/packages/
+  # packages = [ pkgs.git ];
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = "echo hello from $GREET";
+
+  # enterShell = ''
+  #   hello
+  #   git --version
+  # '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    elixir -v
+  '';
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+  languages.elixir = {
+    enable = true;
+  };
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
