f0e3f040449b7d90fccf3d837842c35f5450e2aa
Nate Cox <ncox@covermymeds.com>
2024-07-01 13:09:31 -0700

    Update clojure with lsp

--- templates/dev/clojure/devenv.nix
+++ templates/dev/clojure/devenv.nix
@@ -4,7 +4,7 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = with pkgs; [ clojure-lsp ];
+  # packages = with pkgs; [ ];
 
   # https://devenv.sh/scripts/
   # scripts.hello.exec = "echo hello from $GREET";
@@ -24,9 +24,7 @@
   # services.postgres.enable = true;
 
   # https://devenv.sh/languages/
-  languages.clojure = {
-    enable = true;
-  };
+  languages.clojure.enable = true;
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
